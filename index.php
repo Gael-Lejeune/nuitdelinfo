7ecad285e9d30052a414ddcb0e1ff2a87be98445
Floroux <florian.hocquet@etu.univ-amu.fr>
2019-12-05 20:51:50 +0100

    mojuhiqegriljhgqerzjlhingqer

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title> Index </title>
+    <link rel="stylesheet" href="index.css">
+    <script src="script.js"></script>
+</head>
+<body>
+
+<div class="navbar">
+    <img src="image/zombie.png">
+</div>
+
+</body>
+</html>
