d818f393de9e08f90e47c9fe79bd560cafb0f82d
mickael-maurer <mickael.maurer@etu.univ-amu.fr>
2019-12-05 23:48:07 +0100

    initialisation

---
+++ page/map.php
@@ -0,0 +1,35 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <title>Carte</title>
+    <link rel="icon" type="image/x-icon" href="#">
+
+    <link rel="stylesheet" type="text/css" href="../css/reset.css">
+    <link id="pagestyle" rel="stylesheet" type="text/css" href="../css/map.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <section>
+        <nav>
+            <ul>
+                <li> <a href="#">salle - 117</a> </li>
+                <li> <a href="#">salle - 117</a> </li>
+                <li> <a href="#">salle - 117</a> </li>
+                <li> <a href="#">salle - 117</a> </li>
+                <li> <a href="#">salle - 117</a> </li>
+            </ul>
+        </nav>
+    </section>
+</body>
+</html>
