db71f0efdc8da73963f25f418a6b7cfeeab3340b
Floroux <florian.hocquet@etu.univ-amu.fr>
2019-12-05 23:50:07 +0100

    azerùpk,jazerjkjlazreljnkiazer

---
+++ gameOver2.php
@@ -0,0 +1,20 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title> Index </title>
+
+    <link rel="stylesheet" href="css/gameOver2.css">
+
+    <script src="javascript/gameOver.js"></script>
+</head>
+
+<img class="laPorte1" src="image/Laporte.gif">
+<img class="laPorte2" src="image/Laportereverse.gif">
+<a class="sortir" href="index.php"> ICI </a>
+
+</body onload>
+</html>
--- index.php
+++ index.php
@@ -6,14 +6,33 @@
 <head>
     <meta charset="utf-8">
     <title> Index </title>
-    <link rel="stylesheet" href="index.css">
-    <script src="script.js"></script>
-</head>
-<body>
 
-<div class="navbar">
-    <img src="image/zombie.png">
-</div>
+    <meta http-equiv="refresh" content="16;url=http://latableronde.alwaysdata.net/escapefromiutaix/gameOver.php">
+
+    <link rel="stylesheet" href="css/index.css">
+
+    <script src="javascript/gameOver.js"></script>
+</head>
+<body onload="zombieRun(), setTimeout('perdu()', 5000);">
+
+<img alt="zombie" id="zombieAtt" src="image/Zombiereverse.gif">
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Test escape</title>
+</head>
+
+<a id="bouton1" onclick="test();" >Courir !</a>
+<a style="display: none;" id="bouton2" onclick="test();">Courir !!</a>
+<a style="display: none;" id="bouton3" onclick="test();">Courir !!!</a>
+<a style="display: none;" id="bouton4" onclick="test();">Pas là !</a>
+<a style="display: none;" id="bouton5" onclick="test();">Mais là oui !</a>
+
+
+</body>
+</html>
 
 </body>
 </html>
